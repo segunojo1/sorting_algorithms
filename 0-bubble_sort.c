d9ec1b6947169a356ccd44ddfe5bf495dc3ca68d
segunojo1 <olusegunkd2017@gmail.com>
2024-04-20 18:31:23 +0100

    bubble sort algorithm

---
+++ 0-bubble_sort.c
@@ -0,0 +1,39 @@
+#include "sort.h"
+
+/**
+ * sort - it flips the positions
+ * @px: position of the first
+ * @py: position of the second
+*/
+void sort(int* px, int* py)
+{
+    int save = *px;
+    *px = *py;
+    *py = save;
+}
+
+/**
+ * bubble_sort - does the buble sort
+ * @array: array to sort
+ * @size: size of array
+*/
+void bubble_sort(int *array, size_t size)
+{
+    int i, j;
+    bool sorted;
+
+    for (i = 0; i < size - 1; i++)
+    {
+        for (j = 0; j < i - size - 1; j++)
+        {
+            if (array[j] < array[j + 1])
+            {
+                sort(array[j], array[j + 1]);
+                sorted = true;
+            }
+            sorted = false;
+        }
+        sorted = false;
+        break;
+    }
+}
