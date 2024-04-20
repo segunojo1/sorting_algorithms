881645ba97c3a2be128fb54f29f0219102afc184
segunojo1 <olusegunkd2017@gmail.com>
2024-04-20 18:43:07 +0100

    comment

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -21,10 +21,11 @@ void bubble_sort(int *array, size_t size)
 {
     size_t i, j;
     bool sorted;
-    if (sizeof(array) < 2)
-    {
-        return;
-    }
+    /**if (sizeof(array) < 2)
+     * {
+     *     return;
+     * }
+    */
     
     for (i = 0; i < size - 1; i++)
     {
