5f11d6cc912a888de5484cd658d18fb28a4ca453
segunojo1 <olusegunkd2017@gmail.com>
2024-04-20 18:57:49 +0100

    print after sorting

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -30,6 +30,7 @@ void bubble_sort(int *array, size_t size)
             if (array[j] > array[j + 1])
             {
                 sort(&array[j], &array[j + 1]);
+                print(array);
                 sorted = true;
             }
         }
